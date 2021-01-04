dfee697cd309491c8bd554547ffdb9a11858e7d2
Lexx Ghai <lexxghai@Lexxs-iMac.cgocable.net>
2021-01-04 15:27:44 -0500

    add css

--- light/js/jsfile.js
+++ light/js/jsfile.js
@@ -1,4 +1,24 @@
 
+function myFunction(){
+  var less = document.getElementById("less");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if(less.style.display === "none"){
+    less.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } // close if
+  else{
+    less.style.display =  "none";
+    btnText.innerHTML =  "Read less";
+    moreText.style.display = "inline";
+  } // close else
+}
+
+
+
+
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav-item');
 const myBar = document.getElementById("myHeader");
@@ -19,22 +39,22 @@ navLinks.forEach(link=> {
 
 
 
-window.onscroll = function() {scrollFunction()};
+// window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      // document.getElementById("myHeader").style.top = "0";
-      myBar.style.top= "0";
-      myBar.classList.add("nav-colored"); 
+// function scrollFunction() {
+//     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//       // document.getElementById("myHeader").style.top = "0";
+//       myBar.style.top= "0";
+//       myBar.classList.add("nav-colored"); 
      
-    } 
+//     } 
 
-    else {
-      myBar.style.top = "0px" 
-      myBar.classList.add("nav-transparent");
-      //myBar.classList.remove("nav-colored");
+//     else {
+//       myBar.style.top = "0px" 
+//       myBar.classList.add("nav-transparent");
+//       //myBar.classList.remove("nav-colored");
       
-    }
+//     }
   
-}
+// }
 
