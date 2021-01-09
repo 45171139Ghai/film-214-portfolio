5cd2e6b6cb1bc94ce5f4110b794c7805198b2725
Lexx Ghai <lexxghai@Lexxs-iMac.cgocable.net>
2021-01-09 18:59:12 -0500

    add some css

--- light/js/jsfile.js
+++ light/js/jsfile.js
@@ -17,6 +17,26 @@ function myFunction(){
 }
 
 
+// about Tina implementation
+
+function myFunc(){
+  var less = document.getElementById("read-nothing");
+  var moreText = document.getElementById("read-more");
+  var btnText = document.getElementById("Btn-id");
+
+  if(less.style.display === "none"){
+    less.style.display = "inline";
+    btnText.innerHTML = "More about the instructor";
+    moreText.style.display = "none";
+  } // close if
+  else{
+    less.style.display =  "none";
+    btnText.innerHTML =  "Read less";
+    moreText.style.display = "inline";
+  } // close else
+}
+
+
 
 
 const navToggle = document.querySelector('.nav-toggle');
@@ -58,3 +78,39 @@ navLinks.forEach(link=> {
   
 // }
 
+
+
+
+
+// experimenting with the gallery
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
